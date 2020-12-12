6bb686d86aba0edffc31d7a7051f4d55b56dde79
Roman <pomak803@gmail.com>
2020-12-12 13:14:04 +0300

    Initial commit

--- forms.py
+++ forms.py
@@ -1,12 +1,21 @@
-from flask_wtf import FlaskForm as Form, validators
+from flask_wtf import FlaskForm as Form
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms import StringField, IntegerField, SubmitField
 
-from models import Jockey
+from models import Jockey, Horse, Competition, Participition
 
 
 def get_all_jockeys():
     return Jockey.query.all()
+
+
+def get_all_horses():
+    # return [horse for horse in Jockey.horses]
+    return Horse.query.all()
+
+
+def get_all_cometitions():
+    return Competition.query.all()
 
 
 class JockeyForm(Form):
@@ -14,12 +23,46 @@ class JockeyForm(Form):
     age = IntegerField('Возраст')
     submit = SubmitField('Submit')
 
+    def save(self, request_method):
+        if request_method == 'POST':
+            name = self.name.data
+            age = self.age.data
+            Jockey.save(Jockey(name=name, age=age, win_score=0))
+
 
 class HorseForm(Form):
     name = StringField('Имя')
     age = IntegerField('Возраст')
     jockey = QuerySelectField("Жокей", query_factory=get_all_jockeys)
+    suit = StringField('Масть')
     submit = SubmitField('Submit')
+
+    def save(self, request_method):
+        if request_method == "POST":
+            name = self.name.data
+            age = self.age.data
+            suit = self.suit.data
+            jockey_id = self.jockey.data.id
+            Horse.save(Horse(name=name, age=age, suit=suit, jockey_id=jockey_id, win_score=0))
 
     def __repr__(self):
         return f"HorseForm({self.name},{self.age},{self.submit})"
+
+
+class ParticipationForm(Form):
+    place = StringField('Место')
+    horse = QuerySelectField('Лошадь', query_factory=get_all_horses)
+    jockey = QuerySelectField('Жокей', query_factory=get_all_jockeys)
+    competition = QuerySelectField('Соревнование', query_factory=get_all_cometitions)
+    submit = SubmitField('Submit')
+
+    def save(self, request_method):
+        if request_method == 'POST':
+            place = self.place.data
+            horse = self.horse.data
+            jockey = self.jockey.data
+            competition = self.competition.data
+            Participition.save(
+                Participition(place=place, horse_id=horse.id, jockey_id=jockey.id, competition_id=competition.id))
+
+
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request
 
-import models
-from forms import HorseForm, JockeyForm
+from forms import HorseForm, JockeyForm, ParticipationForm
+from models import Horse, Jockey, Competition, Participition
 
 page = Blueprint('page', __name__, template_folder='templates')
 
@@ -14,26 +14,54 @@ def home_page():
 @page.route('/horses', methods=['GET', 'POST'])
 def horses():
     form = HorseForm(request.form)
-    horses = models.Horse.query.all()
-    if request.method == "POST":
-        name = form.name.data
-        age = form.age.data
-        models.Horse.add_new(name=name, age=age)
+    horses = Horse.query.all()
+    form.save(request.method)
+    return render_template('horses.html', horses=horses, form=form)
+
+
+@page.route('/horses_sorted', methods=['GET', 'POST'])
+def horses_sorted():
+    form = HorseForm(request.form)
+    horses = Horse.query.order_by(Horse.win_score)
+    form.save(request.method)
+    return render_template('horses.html', horses=horses, form=form)
+
+
+@page.route('/horses_sorted_rev', methods=['GET', 'POST'])
+def horses_sorted_rev():
+    form = HorseForm(request.form)
+    horses = Horse.query.order_by(Horse.win_score.desc())
+    form.save(request.method)
     return render_template('horses.html', horses=horses, form=form)
 
 
 @page.route('/jockeys', methods=['GET', 'POST'])
 def jockeys():
     form = JockeyForm(request.form)
-    jockeys = models.Jockey.query.all()
-    if request.method == 'POST':
-        name = form.name.data
-        age = form.age.data
-        models.Jockey.add_new(name=name, age=age)
+    jockeys = Jockey.query.all()
+    form.save(request_method=request.method)
     return render_template('jockeys.html', jockeys=jockeys, form=form)
 
 
-@page.route('/events')
+@page.route('/jockeys_sorted', methods=['GET', 'POST'])
+def jockeys_sorted():
+    form = JockeyForm(request.form)
+    jockeys = Jockey.query.order_by(Jockey.win_score)
+    form.save(request_method=request.method)
+    return render_template('jockeys.html', jockeys=jockeys, form=form)
+
+
+@page.route('/jockeys_sorted_rev', methods=['GET', 'POST'])
+def jockeys_sorted_rev():
+    form = JockeyForm(request.form)
+    jockeys = Jockey.query.order_by(Jockey.win_score.desc())
+    form.save(request_method=request.method)
+    return render_template('jockeys.html', jockeys=jockeys, form=form)
+
+
+@page.route('/events', methods=['GET', 'POST'])
 def events():
-    events = models.Pair.query.all()
-    return render_template('events.html', events=events)
+    form = ParticipationForm(request.form)
+    partitipitions = Participition.query.join(Competition)
+    form.save(request.method)
+    return render_template('events.html', partitipitions=partitipitions, form=form)
